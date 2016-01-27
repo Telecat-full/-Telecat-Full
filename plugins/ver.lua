7236388d957539d65d11430249fdbc8c422fd701
badboyam <pouya001111@gmail.com>
2016-01-27 04:39:27 -0800

    Create ver.lua

---
+++ plugins/ver.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return 'Tele mirror v1'.. [[ 
+This is your group manager
+  This bot CreaTed By:
+  @pouya_x_boy
+  This BoT Is A BesT BoT
+end
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows infernal robot version",
+  patterns = {
+    "^[Vv][Ee][Rr]$"
+  }, 
+  run = run 
+}
+end
