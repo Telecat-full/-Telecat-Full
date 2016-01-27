0939557e86b9981c804d7c0d9813788e2b28c76e
badboyam <pouya001111@gmail.com>
2016-01-27 05:49:17 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -235,7 +235,7 @@ function create_config( )
     "webshot",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {83150569},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Tele mirror v1
